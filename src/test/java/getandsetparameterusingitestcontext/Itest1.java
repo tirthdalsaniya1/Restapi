32733c1cb56dc9ae0a0db3e31415254c0a07a688
tirthblog <133192828+tirthblog@users.noreply.github.com>
2025-01-05 19:16:53 +0530

    [TD] [ updated comment]

--- src/test/java/getandsetparameterusingitestcontext/Itest1.java
+++ src/test/java/getandsetparameterusingitestcontext/Itest1.java
@@ -1,7 +1,6 @@
 package getandsetparameterusingitestcontext;
 
 import org.testng.ITestContext;
-import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 public class Itest1 {
@@ -22,7 +21,7 @@ public class Itest1 {
 
 	@Test()
 	public void getParameter(ITestContext it) {
-//run this using testng other wise it will print null key. We have passed key via itestcontext-testng.xml
+		//run this using testng other wise it will print null key. We have passed key via itestcontext-testng.xml
 		String str = it.getCurrentXmlTest().getParameter("key");
 		// Fetch booking id using ItestContext interface method getAttribute
 		System.out.println("found that key is " + str + " using getParameter method");
